7b1ea9d2c7e70a34c5a598772a11551973e7be5b
Rsbccc <959600086@qq.com>
2024-10-10 16:02:06 +0800

    leetcode 42.TrapRain--hard/TrapRain

--- 42.TrapRain--hard/TrapRain.cpp
+++ 42.TrapRain--hard/TrapRain.cpp
@@ -1,29 +1,37 @@
 #include<iostream>
+#include<stack>
 #include<vector>
-#include<map>
-#include<unordered_map>
 using namespace std;
-
-//FIXME:时间复杂度优化
 
 class Solution {
 public:
     int trap(vector<int>& height) {
-        int sum = 0;
-        unordered_map<int,int> T;
-        for (int i = 0; i < height.size(); i++)
-            for (int k = 1; k <= height[i]; k++)
+        int sum = 0, i = 0;
+        stack<int> S;
+        while (height[i] == 0 && i != height.size() - 1)
+            i++;
+        S.push(i++);
+        while (i < height.size())
+            if (height[i] > height[S.top()] && S.size() >= 2)
             {
-                if (T.find(k) != T.end())
-                    sum += i - T[k] - 1;
-                T[k] = i;
+                int top = S.top();
+                S.pop();
+                int left = S.top();
+                sum += (min(height[i], height[left]) - height[top]) * (i - left - 1);
+            }
+            else
+            {
+                if (height[i] > height[S.top()])
+                    S.pop();
+                S.push(i);
+                i++;
             }
         return sum;
     }
 };
 
 int main(){
-    vector<int> a = {0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1};
+    vector<int> a = {4,2,0,3,2,5};
     Solution pp;
     cout << pp.trap(a);
     return 0;
